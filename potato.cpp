3211d99d30094e5f37a8fe97b9a1dbc0b748d8a5
pseudonym-2669 <55923733+pseudonym-2669@users.noreply.github.com>
2021-07-03 19:25:28 +0200

    Add potato.cpp

---
+++ potato.cpp
@@ -0,0 +1,15 @@
+//c++
+
+#include <iostream>
+
+int main()
+{
+    int compteur(0);
+
+    for (compteur=1; compteur < 11; compteur++)
+    {
+        std::cout << "Potato " << compteur << std::endl;
+    }
+
+    return 0;
+}
